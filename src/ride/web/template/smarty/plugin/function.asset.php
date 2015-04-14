9e8f9d48ada171f6c301cbb7849b1d7032febb48
Kaya <kayalion@kayalion.net>
2015-04-14 16:29:03 +0200

    extended asset function

--- src/ride/web/template/smarty/plugin/function.asset.php
+++ src/ride/web/template/smarty/plugin/function.asset.php
@@ -14,15 +14,29 @@ function smarty_function_asset($params, &$smarty) {
             throw new Exception('Could not add asset: no AssetEntry provided');
         }
         $asset = $params['src'];
+        unset($params['src']);
 
-        if (!isset($params['style']) || !$params['style']) {
-            throw new Exception('Could not add asset: no style parameter provided');
-        }
-        $style = $params['style'];
-
+        $style = null;
+        $transformation = null;
         $var = null;
-        if (!empty($params['var'])) {
+        $default = null;
+
+        if (isset($params['default'])) {
+            $default = $params['default'];
+            $src = $default;
+            unset($params['default']);
+        }
+        if (isset($params['var'])) {
             $var = $params['var'];
+            unset($params['var']);
+        }
+        if (isset($params['transformation'])) {
+            $transformation = $params['transformation'];
+            unset($params['transformation']);
+        }
+        if (isset($params['style']) && $params['style']) {
+            $style = $params['style'];
+            unset($params['style']);
         }
 
         $app = $smarty->getTemplateVars('app');
@@ -32,15 +46,29 @@ function smarty_function_asset($params, &$smarty) {
 
         $dependencyInjector = $app['system']->getDependencyInjector();
         $imageUrlGenerator = $dependencyInjector->get('ride\\library\\image\\ImageUrlGenerator');
+        $image = null;
 
-        $image = $asset->getStyleImage($style);
-        if ($image) {
-            $src = $imageUrlGenerator->generateUrl($image);
-        } else {
+        // check for overriden style image
+        if ($style) {
+            $image = $asset->getStyleImage($style);
+            if ($image) {
+                // get url for the provided image
+                $src = $imageUrlGenerator->generateUrl($image, $transformation, $params);
+            }
+        }
+
+        if (!$image) {
+            // no style image
             $image = $asset->getImage();
+            if (!$image && $default) {
+                $image = $default;
+            }
+
             if (!$image) {
+                // no image resolved
                 $src = null;
             } else {
+                // gather style transformations
                 if (isset($styles[$style])) {
                     $transformations = $styles[$style];
                 } else {
@@ -57,11 +85,21 @@ function smarty_function_asset($params, &$smarty) {
                     $styles[$style->getSlug()] = $transformations;
                 }
 
+                // add template transformation
+                if ($transformation) {
+                    if (isset($transformations[$transformation])) {
+                        unset($transformations[$transformation]);
+                    }
+
+                    $transformations[$transformation] = $params;
+                }
+
+                // get url for the provided image
                 $src = $imageUrlGenerator->generateUrl($image, $transformations);
             }
         }
 
-        if ($var == null) {
+        if ($var === null) {
             return $src;
         } else {
             $smarty->assign($var, $src);
